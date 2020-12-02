d05efac8302ae38562d78cef69a050e57afea773
김기범 <rlqjarhrh@naver.com>
2020-12-02 17:14:52 +0900

    Create 가장 큰 정사각형 찾기.cpp

---
+++ Programmers/가장 큰 정사각형 찾기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> board)
+{
+    int answer = 0;
+    
+    if(board.size() == 1 || board[0].size() == 1)
+        return 1;
+    
+    for(int i = 1; i < board.size();i++){
+        for(int j = 1; j < board[0].size(); j++){
+            if(board[i][j] != 0) {
+                board[i][j] = min(min(board[i - 1][j], board[i][j - 1]),board[i - 1][j - 1]) + 1;
+            }
+            answer = max(answer, board[i][j]);
+        }
+    }
+    
+    return answer * answer;
+}
