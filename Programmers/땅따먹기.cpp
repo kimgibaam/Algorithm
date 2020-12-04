e4af431b5c8c1229d4ec29a18a9d04c63603cf55
김기범 <rlqjarhrh@naver.com>
2020-12-04 17:37:53 +0900

    Create 땅따먹기.cpp

---
+++ Programmers/땅따먹기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    for(int i = 1; i < land.size();i++){
+        for(int j = 0; j < 4; j++){
+            int temp = 0;
+            for(int k = 0; k < 4; k++){
+                if(k == j) continue;
+                temp = max(temp,land[i - 1][k]);    
+            }
+            land[i][j] += temp;
+        }
+    }
+    int idx = land.size() - 1;
+    for(int i =0;i < 4; i++){
+        answer = max(answer, land[idx][i]);
+    }
+    return answer;
+}
