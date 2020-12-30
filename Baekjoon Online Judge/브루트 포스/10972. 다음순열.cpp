ad9d44d7f2579024665388be0ec2938cb3340ae7
김기범 <rlqjarhrh@naver.com>
2020-12-30 15:10:37 +0900

    next_permutation

---
+++ Baekjoon Online Judge/브루트 포스/10972. 다음순열.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+vector<int> num;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		num.push_back(temp);
+	}
+	if (next_permutation(num.begin(), num.end()))
+		for (int i = 0; i < n; i++)
+			cout << num[i] << " ";
+	else cout << "-1";
+	return 0;
+}
