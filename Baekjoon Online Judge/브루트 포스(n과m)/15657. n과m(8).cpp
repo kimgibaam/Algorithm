52c40f7158292f26f1b4bce0cacec31c96b94cf7
kimgibaam <rlqjarhrh@naver.com>
2021-01-08 21:40:32 +0900

    입력, 중복가능 조합

---
+++ Baekjoon Online Judge/브루트 포스(n과m)/15657. n과m(8).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int n, m;
+int arr[8];
+vector<int> picked;
+
+void solve(int cnt, int idx) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << picked[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		picked.push_back(arr[i]);
+		solve(cnt + 1, i);
+		picked.pop_back();
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	solve(0, 0);
+
+	return 0;
+}
