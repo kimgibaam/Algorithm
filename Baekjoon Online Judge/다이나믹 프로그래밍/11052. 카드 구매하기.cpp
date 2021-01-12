71b072086873f154bcc52b9fe107b129c16e36d4
kimgibaam <rlqjarhrh@naver.com>
2021-01-12 09:55:00 +0900

    dp는 뒤부터

---
+++ Baekjoon Online Judge/다이나믹 프로그래밍/11052. 카드 구매하기.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int num[1001];
+int dp[1001];
+
+// n개 뽑았을 때의 최대값은 n - 1 + 1
+//  n - 2 + 2 .... n - N + N 중 최대값
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> num[i];
+	}
+
+	dp[1] = num[1];
+	for (int i = 2; i <= N; i++) {
+
+		int temp = num[i];
+		for (int j = 1; i - j > 0; j++) {
+			temp = max(temp, dp[i - j] + dp[j]);
+		}
+		dp[i] = temp;
+	}
+
+	cout << dp[N];
+
+	return 0;
+}
