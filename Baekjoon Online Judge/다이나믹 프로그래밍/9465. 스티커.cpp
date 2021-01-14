f78d0987f37d8c12e2112f46be4c637fdcaccfac
kimgibaam <rlqjarhrh@naver.com>
2021-01-14 10:56:42 +0900

    두칸뒤 대각선까지만!

---
+++ Baekjoon Online Judge/다이나믹 프로그래밍/9465. 스티커.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int arr[2][100001];
+int dp[2][100001];
+
+int main() {
+	int t;
+	cin >> t;
+	for (int tc = 0; tc < t; tc++) {
+		int n;
+		cin >> n;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j <= n; j++) {
+				cin >> arr[i][j];
+			}
+		}
+
+		dp[0][1] = arr[0][1];
+		dp[1][1] = arr[1][1];
+
+		for (int i = 2; i <= n; i++) {
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+		}
+		cout << max(dp[0][n], dp[1][n]) << endl;
+	}
+
+	return 0;
+}
