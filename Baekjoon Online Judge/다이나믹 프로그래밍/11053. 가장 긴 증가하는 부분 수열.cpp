4759c3d0108e942b26593354643f22d289b79d0c
kimgibaam <rlqjarhrh@naver.com>
2021-01-15 09:48:34 +0900

    낮은 값만 체크

---
+++ Baekjoon Online Judge/다이나믹 프로그래밍/11053. 가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int arr[1000];
+int dp[1000];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+	
+	for (int i = 1; i < n; i++) {
+		int Max = arr[i];
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < Max)
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+		ans = max(dp[i], ans);
+	cout << ans;
+
+	return 0;
+}
