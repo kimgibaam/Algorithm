46f3c121c44aa19d16a06a21069663920ce71e87
kimgibaam <rlqjarhrh@naver.com>
2021-01-16 09:31:49 +0900

    증가하는 소스에서 부등호 방향만 바꿈

---
+++ Baekjoon Online Judge/다이나믹 프로그래밍/11722. 가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int arr[1000];
+int dp[1000];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+	
+	for (int i = 1; i < n; i++) {
+		int Max = arr[i];
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > Max)
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+		ans = max(dp[i], ans);
+	cout << ans;
+		
+	return 0;
+}
